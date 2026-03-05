bc0e829c920a1d1668dbc96487329412ffb68cec
syedsajid <sajidsyedhafizsajidsyed@gmail.com>
2026-03-04 19:09:42 -0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
+import ProjectTimeline from "@/components/projects/project-themeline";
+import { useProducts } from "@/hooks/use-products";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Footer/>
         <Navbar/>
         {children}
       </body>
